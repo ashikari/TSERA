19f034c3e64507c15a3726f02fa12f44a07c2407
ashikari <ashikari123@gmail.com>
2019-01-18 23:18:58 -0500

    updated design calculations

--- Actuator_force.py
+++ Actuator_force.py
@@ -44,15 +44,15 @@ def theta_min(l_r):
 
 theta_max = 68/180*np.pi
 
-def E_ratio(l_b,l_o, l_total):
-    return L_theta(theta_max, l_b, l_total)/(L_theta(theta_min(l_o), l_b, l_total) + l_b)
+def E_ratio(l_b,l_o, l_total, offset = 0):
+    return (L_theta(theta_max, l_b, l_total) +offset)/(L_theta(theta_min(l_o), l_b, l_total) + offset)
 
 def F_nut(theta, F_applied,l_bent, l_r, l_total ):
     return F_applied/2.0*( l_total*np.cos(theta) - l_bent*np.sin(theta) ) 			\
 					/(l_r*np.sin(theta) + l_bent*np.cos(theta) )
 
-# F_req = lambda theta:  F_applied/2.0*( l_total*np.cos(theta) - l_bent*np.sin(theta) ) 			\
-# 					/(l_r*np.sin(theta) + l_bent*np.cos(theta) )
+def T_motor(theta, F_applied, l_bent, l_r, l_total, z_s = zeta_s, z_p = zeta_p):
+    return lead*F_nut(theta, F_applied,l_bent, l_r, l_total )/(np.pi*z_s* z_p)
 
 #-----------------------------------------------------
 
@@ -69,10 +69,11 @@ I_base = I_com + I_stage1 + I_stage2
 omega = (np.pi/2.0)/5.0
 alpha = omega/2.0
 g = 9.8
-m_stage = 339.5/1000.0
+m_stage = 865/1000.0
 
 
 F_app = 1/(168.24/1000.0)*(I_base*alpha + m_stage*g*L_max/1000.0 + 2*m_stage*g*L_max/1000.0)
+T_stall = T_motor(theta_min(l_r), F_app, l_bent, l_r, l_total, zeta_s, zeta_p)*1.2
 
 def w_l(L, l_bent, l_o, l_e):
     a1 = 2.0/(l_bent**2+ l_e**2)
@@ -80,7 +81,31 @@ def w_l(L, l_bent, l_o, l_e):
     a3 = (L*l_bent)*(l_e - l_o)
     return a1*(a2-a3)
 
+def theta_w(w, lb, lo):
+    a = 2*(lb**2)*lo
+    b = (lb**2)*(lb**2 + 4*lo**2 - w**2)
+    c = lb**3 - lb*w**2
+    return np.arctan( (a-w*np.sqrt(b))/c )
 
+
+def dphi_dt(phi, wnl):
+    w = w_max - lead*1000*phi/np.pi
+    T = T_motor(theta_w(w, l_bent, l_r), F_app, l_bent, l_r, l_total, zeta_s, zeta_p)
+    return -wnl*(T/T_stall - 1)
+
+
+def sim_phi(wnl, dt):
+    phi_f = (w_max - w_min)*np.pi
+    phi = 0
+    count = 0
+    
+    print ("phi_f: " + str(phi_f) )
+    while phi<phi_f:
+        phi = phi + dphi_dt(phi, wnl)*dt
+#         print(phi)
+        count+=1
+    return count*dt
+        
 def print_stats():
     print("F_nut: " + str( F_nut(theta_min(l_r), F_app, l_bent, l_r, l_total) ) )
     print("F_act: " + str(F_act) )
@@ -96,3 +121,5 @@ def print_stats():
     
     print("alpha: " + str(alpha) )
     print("F_app Grav Approximation: " + str(F_app) )
+    
+    print("Peak Motor Torque Requirement: "+ str(T_motor(theta_min(l_r), F_app, l_bent, l_r, l_total, zeta_s, zeta_p)) )
